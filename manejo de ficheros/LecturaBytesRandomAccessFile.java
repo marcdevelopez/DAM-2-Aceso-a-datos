8ad48ae75a505f8319b029c19b7b4b5447046152
Marcos Garcia <143760248+marcdevelopez@users.noreply.github.com>
2023-10-06 23:51:17 +0200

    agregado a clase .java manejo de ficheros/LecturaBytesRandomAccessFile.java lectura de array de bytes de un fichero.

--- manejo de ficheros/LecturaBytesRandomAccessFile.java
+++ manejo de ficheros/LecturaBytesRandomAccessFile.java
@@ -5,7 +5,12 @@ import java.io.File;
 public class LecturaBytesRandomAccessFile {
 
 	public static void main(String[] args) {
-		// creamos primero un fichero con los datos a leer
+		/*
+		 * LECTURA DE UN BYTE
+		 * 
+		 * Creamos un fichero con los datos a leer
+		 */
+		
 		File pruebaFichero = new File("salida.txt");
 		try {
 			pruebaFichero.createNewFile();
@@ -22,11 +27,11 @@ public class LecturaBytesRandomAccessFile {
 		}
 		try {
 			/*
-			 * RandomAccessFile posee dos constructores, en ambos constructores el segundo
-			 * parámetro es el mismo: el modo de acceso. Sin embargo, para el primer
-			 * parámetro tenemos la versión del constructor en la que podemos introducir el
-			 * objeto File directamente o, como en el caso de la imagen superior, introducir
-			 * la ruta del fichero directamente como String.
+			 * RandomAccessFile posee dos constructores: En ambos constructores el segundo
+			 * parámetro es el mismo: el modo de acceso.
+			 * Sin embargo, para el primer parámetro tenemos la versión del constructor en
+			 * la que podemos introducir el objeto File directamente o, como en este caso,
+			 * introducir la ruta del fichero directamente como String.
 			 */
 			RandomAccessFile file = new RandomAccessFile("salida.txt", "r");
 			/*
@@ -46,7 +51,48 @@ public class LecturaBytesRandomAccessFile {
 			 */
 			int unByte = file.read();
 			System.out.println(
-					"El byte leído en el puntero " + String.valueOf(posicionPuntero) + " es: " + (char) unByte);
+					"\nEl byte leído en el puntero " + String.valueOf(filePointer) + " es: " + (char) unByte);
+
+			/*
+			 * LECTURA DE UN ARRAY DE BYTES (CONJUNTO DE BYTES)
+			 * 
+			 * Si en lugar de leer un byte, lo que nos interesa realmente es leer una
+			 * cantidad determinada de bytes, podremos realizar esta implementación:
+			 * 
+			 * En primer lugar, crearemos un array de bytes, en concreto la longitud del
+			 * File pruebaFichero en bytes.
+			 */
+			byte[] arrayBytes = new byte[(int) pruebaFichero.length()];
+			/*
+			 * Definimos la posición del puntero y el tamaño de bytes que queremos extraer
+			 * de nuestro fichero. Lo pondremos a 0, ya que el método seek() ya situa el
+			 * puntero donde empezar queremos que comience a leer...
+			 */
+			int inicioPunteroMetodoRead = 0;
+			// definimos la cantidad de bytes a leer; queremos leer la palabra "fichero".
+			int size = 7;
+			/*
+			 * Por último, usamos el método read(), que en este caso, acepta los siguientes
+			 * parámetros:
+			 * 
+			 * • Primer parámetro: el array de bytes dónde se va a almacenar la información,
+			 * • Segundo parámetro: el inicio o posición en la que comenzaremos a leer. 
+			 * • Último parámetro: la cantidad máxima de bytes a extraer.
+			 * 
+			 * Modificamos el puntero de lectura del RandomAccessFile para comenzar a leer
+			 * el primer byte (carácter) donde elijamos:
+			 */
+			long inicioPunteroLecturaSeek = 11;
+			file.seek(inicioPunteroLecturaSeek);
+			int bytesLeidos = file.read(arrayBytes, inicioPunteroMetodoRead, size);
+			// A continuación recorremos el array y lo imprimimos
+			System.out.println("\n" + size + " bytes leídos, desde el puntero " + inicioPunteroLecturaSeek + ":");
+			for (int n : arrayBytes) {
+				System.out.print((char) n);
+			}
+
+			// cerramos el flujo del objeto RandomAccessFile para no desperdiciar recursos
+			file.close();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -58,3 +104,4 @@ public class LecturaBytesRandomAccessFile {
 	}
 
 }
+
